6dc5ad14a006e30d5eca30fd9975713e0b8dca20
karimoreira <kari.moreira@cs.cruzeirodosul.edu.br>
2025-01-26 10:46:18 -0300

    feat: melhorando js para a rua com função autoinvocavel (iife)

--- elevador/js/road.js
+++ elevador/js/road.js
@@ -1,5 +1,15 @@
-const elementosFaixas = document.querySelectorAll('[faixas]')
-elementosFaixas.forEach(el => {
-    const quantidade = el.getAttribute('faixas')
-    
-})
+(function () {
+  function criarFaixas() {
+    const elementosFaixas = document.querySelectorAll("[faixas]");
+    elementosFaixas.forEach((el) => {
+      const quantidade = +el.getAttribute("faixas");
+      for (let i = 0; i < quantidade; i++) {
+        const faixa = document.createElement("div");
+        faixa.classList.add("faixa");
+        el.appendChild(faixa);
+      }
+    });
+  }
+
+  criarFaixas();
+})();
